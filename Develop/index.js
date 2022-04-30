6591aa483c3644c241e1d4e1ba9543e0f3d0b3a4
Jonathan Price <jonprice0@gmail.com>
2022-04-30 13:51:01 -0400

    imported packages to index.js and added questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,66 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions and examples for use:'
+    },
+    {
+       type: 'input',
+       name: 'contribute',
+       mesage: 'Please provide guidelines for developers who want to contribute to this project:' 
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide explicit instructions on how to run necessary tests:'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select a license(s) for this project:',
+        choices: ['MIT', 'Apache', 'Eclipse']
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+    .then(userInput => console.log(userInput));
+};
 
 // Function call to initialize app
 init();
